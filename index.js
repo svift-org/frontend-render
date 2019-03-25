40937fce5b3799356e69b7dd45ccbefd4359b0bb
Sebastian Meier <contact@sebastianmeier.eu>
2019-03-26 00:52:02 +0100

    attr css

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ SVIFT.render.drawPNG = function(){
     var styles = window.getComputedStyle(that.node());
 
     for(key in styles){
-      if(isNaN(key) && key !== 'transform'){
+      if(isNaN(key) && key !== 'transform' && key !== 'x' && key !== 'y'){
         if(that.attr(key) !== null){
           styles[key] = that.attr(key);
         }
