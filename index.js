20c3f6377ce07c67e7c26aea56351a30b3b85149
Sebastian Meier <contact@sebastianmeier.eu>
2019-03-27 00:04:56 +0100

    gif implementation

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ SVIFT.render = function(){
   var state = {
     init: false,
     setup: false,
+    gif: false,
+    gifStep: 0
   },
   width = 500, 
   height = 500,
   container,
-  data, vis;
+  data, vis, gif, storage = {};
 
   var css = [
     ["fill","fill"],
@@ -22,6 +24,160 @@ SVIFT.render = function(){
     ["stroke","stroke"],
     ["stroke-width","strokeWidth"]
   ];
+
+  var config = {
+    "video":{
+      "size":{
+        "width":500,
+        "height":500
+      },
+      "scale":{
+        "width":500,
+        "height":500
+      },
+      frames: 100
+    },
+    "sizes":[
+      {
+        "file": "instagram",
+        "name": "Instagram",
+        "size":{
+          "width":540,
+          "height":540
+        },
+        "scale":{
+          "width":1080,
+          "height":1080
+        }
+      },
+      {
+        "file": "linkedin",
+        "name":"LinkIn",
+        "size":{
+          "width":700,
+          "height":400
+        },
+        "scale":{
+          "width":1400,
+          "height":800
+        }
+      },
+      {
+        "file": "google",
+        "name":"Google+",
+        "size":{
+          "width":497,
+          "height":373
+        },
+        "scale":{
+          "width":994,
+          "height":746
+        }
+      },
+      {
+        "file": "tumblr",
+        "name":"Tumblr",
+        "size":{
+          "width":640,
+          "height":960
+        },
+        "scale":{
+          "width":1280,
+          "height":1920
+        }
+      },
+      {
+        "file": "pinterest",
+        "name":"Pinterest",
+        "size":{
+          "width":600,
+          "height":600
+        },
+        "scale":{
+          "width":1200,
+          "height":1200
+        }
+      },
+      {
+        "file": "twitter",
+        "name":"Twitter",
+        "size":{
+          "width":512,
+          "height":256
+        },
+        "scale":{
+          "width":1024,
+          "height":512
+        }
+      },
+      {
+        "file": "snapchat",
+        "name":"Snapchat",
+        "size":{
+          "width":476,
+          "height":810
+        },
+        "scale":{
+          "width":952,
+          "height":1620
+        }
+      },
+      {
+        "file": "facebook",
+        "name":"Facebook",
+        "size":{
+          "width":600,
+          "height":315
+        },
+        "scale":{
+          "width":1200,
+          "height":630
+        }
+      },
+      {
+        "file": "horizontal",
+        "name":"Horizontal / Landscape",
+        "size":{
+          "width":1024,
+          "height":512
+        },
+        "scale":{
+          "width":2048,
+          "height":1024
+        }
+      },
+      {
+        "file": "vertical",
+        "name":"Vertical / Portrait",
+        "size":{
+          "width":800,
+          "height":1200
+        },
+        "scale":{
+          "width":1600,
+          "height":2400
+        }
+      },
+      {
+        "file": "square",
+        "name":"Square",
+        "size":{
+          "width":512,
+          "height":512
+        },
+        "scale":{
+          "width":1024,
+          "height":1024
+        }
+      }
+    ]
+  };
+
+  var configKeys = {};
+
+  config.sizes.forEach(function(size, i){
+    configKeys[size.file] = i;
+  });
 
  /*
   * This function sets up an offscreen rendering canvas and svg
@@ -54,6 +210,10 @@ SVIFT.render = function(){
 
     container.select('svg').remove();
 
+    // if(custom){
+    //   data["custom"] = custom;
+    // }
+
     vis = SVIFT.vis[data.vis.type](data, container);
     vis.setScale(true);
     vis.init();
@@ -74,6 +234,11 @@ SVIFT.render = function(){
   */
   module.resizeSVG = function(pixelWidth, pixelHeight, renderWidth, renderHeight){
     if(state.setup){
+
+      d3.select('#offscreen-svg svg')
+        .style('width', "100%")
+        .style('height', "100%");
+
       vis.setScale(true);
 
       container
@@ -93,16 +258,7 @@ SVIFT.render = function(){
     }
   };
 
-
-  /*
-  * Copy the current SVG to the PNG
-  */
-  module.drawPNG = function(){
-
-    d3.select('#offscreen-svg svg')
-      .style('width', 500)
-      .style('height', 500);
-
+  module.inlineCSS = function() {
     d3.selectAll('#offscreen-svg svg g, #offscreen-svg svg text, #offscreen-svg svg path, #offscreen-svg svg rect, #offscreen-svg svg circle, #offscreen-svg svg line, #offscreen-svg svg ellipse, #offscreen-svg svg tspan, #offscreen-svg svg title').each(function(d,i){
       var that = d3.select(this);
       var styles = window.getComputedStyle(that.node());
@@ -115,15 +271,31 @@ SVIFT.render = function(){
         that.style(c[0], value);
       });
     });
+  };
 
-    module.toDataURL(function(data) {
+  /*
+  * provide a filename from the config and a download will be created
+  */
+  module.downloadPNG = function(type){
+    if (state.setup && state.running && !state.gif) {
 
-      var link = document.createElement('a');
-      link.download = "my-image.png";
-      link.href = data;
-      link.click();
+      let tConfig = config.sizes[configKeys[type]];
+      module.resizeSVG(tConfig.size.width, tConfig.size.height, tConfig.scale.width, tConfig.scale.height);
+      module.drawSVG(1);
+      module.inlineCSS();
 
-    });
+      module.toDataURL(function(data) {
+
+        //.replace("image/png", "image/octet-stream")
+        var link = document.createElement('a');
+        link.download = type + ".png";
+        link.href = data;
+        link.click();
+
+      });
+    } else {
+      console.log('Either render not initialised or currently rendering a GIF.');
+    }
   };
 
   /*
@@ -136,67 +308,65 @@ SVIFT.render = function(){
   };
 
   /*
-  * Store the PNG data in the SVIFT.render.state object for quick download access
-  */
-  module.storePNG = function(key){
-  };
-
-  /*
-  * Generate a download request for the data, returns false if image does not exist
-  */
-  module.downloadPNG = function(key){
-
-  };
-
-  /*
-
-  var canvas = document.getElementById("mcanvas");
-    image = canvas.toDataURL("image/png", 1.0).replace("image/png", "image/octet-stream");
-    var link = document.createElement('a');
-    link.download = "my-image.png";
-    link.href = image;
-    link.click();
-
-  */
-
-  /*
   * Generate a gif
   */
   module.buildGif = function(){
-    var gif = new GIF({
-      workers: 3,
-      quality: 0.5,
-      repeat: 0,
-      width: 500,
-      height: 500
-    }).on("progress", function (p) {
-      console.log("progress", p);
-    }).on("finished", function (blob) {
-      console.log("finished");
-      d3.select('#container').append('img')
-        .attr("src",URL.createObjectURL(blob))
-        .style('width', 500)
-        .style('height', 500);
-    });
+    if (state.setup && state.running && !state.gif) {
 
-    /*let img = new Image();
+      state.gif = true;
+      state.gifStep = 0;
+
+      module.resizeSVG(config.video.size.width, config.video.size.height, config.video.scale.width, config.video.scale.height);
+      vis.reset();
+
+      gif = new GIF({
+        workers: 3,
+        quality: 0.5,
+        repeat: 0,
+        width: config.video.size.width,
+        height: config.video.size.height
+      }).on("progress", function (p) {
+        console.log("progress", p);
+      }).on("finished", function (blob) {
+        console.log("finished");
+        
+        var link = document.createElement('a');
+        link.download = "animation.gif";
+        link.href = URL.createObjectURL(blob);
+        link.click();
+
+      });
+
+      module.addFrame();
+  };
+
+  module.addFrame = function() {
+    module.drawSVG(state.gifStep/config.video.frames);
+    module.inlineCSS();
+
+    module.toDataURL(function(data) {
+
+      var img = new Image();
       img.crossOrigin = "*";
-      
-      img.onload = function(){
+        
+      img.onload = function() {
         gif.addFrame(img, {
           delay: 1,
           copy: true
         });
 
-        added++;
-        if(added == 9){
+        state.gifStep++;
+
+        if(state.gifStep >= config.video.frames){
           gif.render();
         }else{
-          addFrame();
+          module.addFrame();
         }
       };
 
-      img.src = data; */
+      img.src = data;
+
+    });
   };
 
   /*
@@ -211,65 +381,28 @@ SVIFT.render = function(){
   module.getStatus = function(){
   };
 
+  /*
+
+  DOWNLOAD SVG
+
+  var svg_root = document.getElementById('your_svg_root_element_here');
+var svg_source = svg_root.outerHTML;
+var svg_data_uri = 'data:image/svg+xml;base64,' + btoa(svg_source);
+var link = document.getElementById('anchor_element');
+link.setAttribute('href', svg_data_uri);
+
+  */
+
   return module;
 
 }();
 
 /*
 
-function clear(){
-    d3.select('#container').selectAll('*').remove();
-    return true;
-  }
-
-  var custom = false;
-
-  // CUSTOMJSON
-
   var v;
-
-  function vis(data){
-    clear();
-    if(custom){
-      data["custom"] = custom;
-    }
-    v = SVIFT.vis[data.vis.type](data, d3.select('#container'));
-    v.init();
-    return true;
-  }
-
-  function init(step){
-    console.log('vis.html:init:'+step);
-    v.goTo(step);
-    return true;
-  }
-
-  function play(){
-    v.play();
-    return true;
-  }
-
-  function reset(){
-    v.reset();
-    return true;
-  }
-
-  function goto(t){
-    v.goTo(t);
-    return true;
-  }
-
-  function setScale(s){
-    v.setScale(s);
-    return true;
-  }
 
   function resize(){
     v.preResize();
-    return true;
-  }
-
-  function callback(){
     return true;
   }
 
@@ -319,150 +452,3 @@ function clear(){
   }
 
 */
-
-SVIFT.render.config = {
-	"video":{
-		"size":{
-			"width":500,
-			"height":500
-		},
-		"output":{
-			"width":500,
-			"height":500
-		}
-	},
-	"sizes":[
-		{
-			"file": "instagram",
-			"name": "Instagram",
-			"size":{
-				"width":540,
-				"height":540
-			},
-			"scale":{
-				"width":1080,
-				"height":1080
-			}
-		},
-		{
-			"file": "linkedin",
-			"name":"LinkIn",
-			"size":{
-				"width":700,
-				"height":400
-			},
-			"scale":{
-				"width":1400,
-				"height":800
-			}
-		},
-		{
-			"file": "google",
-			"name":"Google+",
-			"size":{
-				"width":497,
-				"height":373
-			},
-			"scale":{
-				"width":994,
-				"height":746
-			}
-		},
-		{
-			"file": "tumblr",
-			"name":"Tumblr",
-			"size":{
-				"width":640,
-				"height":960
-			},
-			"scale":{
-				"width":1280,
-				"height":1920
-			}
-		},
-		{
-			"file": "pinterest",
-			"name":"Pinterest",
-			"size":{
-				"width":600,
-				"height":600
-			},
-			"scale":{
-				"width":1200,
-				"height":1200
-			}
-		},
-		{
-			"file": "twitter",
-			"name":"Twitter",
-			"size":{
-				"width":512,
-				"height":256
-			},
-			"scale":{
-				"width":1024,
-				"height":512
-			}
-		},
-		{
-			"file": "snapchat",
-			"name":"Snapchat",
-			"size":{
-				"width":476,
-				"height":810
-			},
-			"scale":{
-				"width":952,
-				"height":1620
-			}
-		},
-		{
-			"file": "facebook",
-			"name":"Facebook",
-			"size":{
-				"width":600,
-				"height":315
-			},
-			"scale":{
-				"width":1200,
-				"height":630
-			}
-		},
-		{
-			"file": "horizontal",
-			"name":"Horizontal / Landscape",
-			"size":{
-				"width":1024,
-				"height":512
-			},
-			"scale":{
-				"width":2048,
-				"height":1024
-			}
-		},
-		{
-			"file": "vertical",
-			"name":"Vertical / Portrait",
-			"size":{
-				"width":800,
-				"height":1200
-			},
-			"scale":{
-				"width":1600,
-				"height":2400
-			}
-		},
-		{
-			"file": "square",
-			"name":"Square",
-			"size":{
-				"width":512,
-				"height":512
-			},
-			"scale":{
-				"width":1024,
-				"height":1024
-			}
-		}
-	]
-};
